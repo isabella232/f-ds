bfb88635315cf84bebdf34b1d3ad0015a990a4d0
Paul Merrill <pdm@pdm.me>
2015-04-04 15:32:10 -0700

    serve legalDocs/ for toupp and dmca

--- routes.js
+++ routes.js
@@ -15,6 +15,6 @@ module.exports = function(router) {
   router.get('/story/create', renderStatic('create.html'))
   router.get('/story/:story', renderStatic('story.html'))
   router.get('/about', renderStatic('about.html'))
-  router.get('/about/toupp', renderStatic('toupp.html'))
-  router.get('/about/dmca', renderStatic('dmca.html'))
+  router.get('/about/toupp', renderStatic('legalDocs/toupp.html'))
+  router.get('/about/dmca', renderStatic('legalDocs/dmca.html'))
 }
