758821f40a44ef3934859c96ee485112b776ca8a
Paul Merrill <pdm@pdm.me>
2015-04-04 22:54:49 -0700

    config: implement all backend API

--- routes/feed.js
+++ routes/feed.js
@@ -1,25 +1,19 @@
+'use strict';
+
+var BackendAPI = require('../config/backend/api')
 
 module.exports = function(router) {
-
-  'use strict';
-
-  var API = require('../models/api')
-
-  function handleFeedFetch(req, res) {
-    API.feed.get(function(err, feed) {
+  router.get('/feed', function(req, res) {
+    BackendAPI.feed.get({}, function(err, clientErr, feed) {
       if (err) {
-
-        // (TEMP)
-        console.error(err)
+        console.error(err.stack)
+        res.render('500.html')
+      } else if (clientErr) {
+        console.trace('Error: client error on feed: ' + clientErr)
+        res.render('500.html')
       } else {
-
-        res.render('feedView.html', { feed: feed })
-
+        res.render('feed.html', { feed: feed.feed })
       }
-
     })
-  }
-
-  router.get('/feed', handleFeedFetch)
-
+  })
 }
