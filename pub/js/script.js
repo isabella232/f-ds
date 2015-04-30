d7d12d9b4788c1274be098c5ff96f88fb311d1a2
waazup2 <brandonlchai@gmail.com>
2015-04-29 21:00:50 -0700

    removed javascript for modal links and made login/signup views consistent

--- pub/js/script.js
+++ pub/js/script.js
@@ -11,20 +11,3 @@ function checkPass(){
     pass2.setCustomValidity('');
   }
 }
-
-
-// changes modal links to static page links for screen widths under 480px
-
-function setup_for_width(mql) {
-  if (mql.matches) {
-    $('#welcome-buttons > :nth-child(1)').attr('href', '#signup')
-    $('#welcome-buttons > :nth-child(2)').attr('href', '#login')
-  } else {
-    $('#welcome-buttons > :nth-child(1)').attr('href', '/user/signupn')
-    $('#welcome-buttons > :nth-child(2)').attr('href', '/user/login')
-  }
-}
-
-var width_mql = window.matchMedia("(min-width: 480px)");
-width_mql.addListener(setup_for_width);
-setup_for_width(width_mql);
