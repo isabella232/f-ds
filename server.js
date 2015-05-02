df3f3f8c8ac68cfc4bedb3fba6225c64e49e4a2f
Christopher Banh <chrisngobanh@gmail.com>
2015-05-02 16:31:52 -0700

    Changed the way Express serves static files to server from Bootstrap directly, instead of the entire node_modules folder

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ env.express(app)
 // Tell Express to serve static objects from these directories
 app.use(express.static('pub'))
 app.use(express.static('node_modules/css-modal/build'))
-app.use(express.static('node_modules'))
+app.use(express.static('node_modules/bootstrap/dist'))
 
 // Set up routes.
 var router = express.Router()
