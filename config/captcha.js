8665123b507c111b80222a32b2b947ceeed734b9
Christopher Banh <chrisngobanh@gmail.com>
2015-07-01 17:49:41 -0700

    Captcha added to Signup page and modal Created config/captcha.js, which exports a function to verify a captcha and a bool that depends on whether or not the CAPTCHA_KEY environment variable is defined in Development mode. Refactored the userCreate function to check if a Captcha is valid (if it's defined) before finishing the rest of the userCreate process.

---
+++ config/captcha.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var request = require('request')
+
+var CAPTCHA_KEY = process.env.CAPTCHA_KEY
+
+if (!CAPTCHA_KEY) {
+  if (process.env.NODE_ENV === 'production' ) {
+    throw new Error('Captcha Secret Key is required for signups.')
+  } else {
+    console.trace('Warning: Captcha Secret Key is not defined. ' +
+                  'Captcha will have no effect on sign ups.')
+
+  }
+}
+
+function verifyCaptcha(key, callback) {
+  var form =
+    { secret  : CAPTCHA_KEY
+    , response: key
+    }
+  request.post(
+    { url : 'https://www.google.com/recaptcha/api/siteverify'
+    , form: form
+    , json: true
+    }
+  , function(err, res, body) {
+      callback(err, body.success)
+    }
+  )
+}
+
+module.exports = { verify: verifyCaptcha, CAPTCHA_KEY: CAPTCHA_KEY }
