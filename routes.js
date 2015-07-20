3b8b13218cd44cfa6ad3c36c8752deec977b9b6c
waazup2 <brandonlchai@gmail.com>
2015-07-19 23:12:15 -0700

    fixed bug in results section when total votes is 0 and removed transitions

--- routes.js
+++ routes.js
@@ -161,8 +161,14 @@ function renderStory(req, res) {
                 question.totalVotes += question.answers[i].votes
               }
               // Calculate percentage of total votes for each answer option
-              for (var i = 0; i < question.answers.length; i++) {
-                question.answers[i].percent = Math.round(question.answers[i].votes/question.totalVotes*100);
+              if (question.totalVotes > 0){
+                for (var i = 0; i < question.answers.length; i++) {
+                  question.answers[i].percent = Math.round(question.answers[i].votes/question.totalVotes*100);
+                }
+              } else {
+                question.answers.forEach(function(el, i, arr) {
+                  arr[i].percent = 0;
+                })
               }
               res.render(
                 'story.html'
