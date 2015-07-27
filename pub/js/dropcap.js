30a1b223ae35cc4c0d9f305a5be04c2befa4e718
waazup2 <brandonlchai@gmail.com>
2015-07-26 23:48:39 -0700

    implemented dropcap with bbcode

--- pub/js/dropcap.js
+++ pub/js/dropcap.js
@@ -1,20 +1,25 @@
-// Drop cap settings
+// Drop cap implementation
+
+var xbbcode = document.getElementById('xbbcode')
+
+// if bbcode is not used in the narrative, implement the dropcap as follows
+if ( xbbcode.innerHTML[0] != "<" && xbbcode.innerHTML[0] != "[" ){
+  document.getElementsByClassName('definition')[0].childNodes[0].innerHTML = narrative.substr(0,1)
+  document.getElementsByClassName('definition')[0].childNodes[0].id = 'drop-cap'
+} else if (xbbcode.innerHTML[0] === '<') { // otherwise, implement the dropcap with this hack
+  // create new span element to store the dropcap and append to the beginning of xbbcode
+
+  var newSpan = document.createElement('SPAN')
+  var firstLetter = document.createTextNode(xbbcode.childNodes[0].textContent.substr(0,1))
+  newSpan.appendChild(firstLetter)
+  newSpan.id = 'drop-cap'
+  xbbcode.insertBefore(newSpan, xbbcode.childNodes[0])
+  newSpan.style.textTransform = 'uppercase'
+  // delete the leading character in the narrative used as the dropcap
+  xbbcode.childNodes[1].textContent = xbbcode.childNodes[1].textContent.substr(1)
+}
 
 var dropcap = document.querySelector("#drop-cap");
-// media query event handler
-if (matchMedia) {
-  var mq = window.matchMedia("(min-width: 768px)");
-  mq.addListener(WidthChange);
-  WidthChange(mq);
-}
-// media query change
-function WidthChange(mq) {
-  if (mq.matches) {
-    // if window width is at least 768px, the drop cap spans the height of 3 lines
-    window.Dropcap.layout(dropcap, 3);
-  }
-  else {
-    // if window width is less than 768px, the drop cap spans the height of 2 lines
-    window.Dropcap.layout(dropcap, 2);
-  }
-}
+// the dropcap spans the height of 3 lines
+window.Dropcap.layout(dropcap, 3);
+
