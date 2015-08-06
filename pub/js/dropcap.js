85e8e2998e3d3e7dee0bf4b82f9c4db50d61e6d9
waazup2 <brandonlchai@gmail.com>
2015-08-05 21:24:30 -0700

    force drop cap to be uppercase

--- pub/js/dropcap.js
+++ pub/js/dropcap.js
@@ -4,13 +4,13 @@ var xbbcode = document.getElementById('xbbcode')
 
 // if bbcode is not used in the narrative, implement the dropcap as follows
 if ( xbbcode.innerHTML[0] != "<" && xbbcode.innerHTML[0] != "[" ){
-  document.getElementsByClassName('definition')[0].childNodes[0].innerHTML = narrative.substr(0,1)
+  document.getElementsByClassName('definition')[0].childNodes[0].innerHTML = narrative.substr(0,1).toUpperCase()
   document.getElementsByClassName('definition')[0].childNodes[0].id = 'drop-cap'
 } else if (xbbcode.innerHTML[0] === '<') { // otherwise, implement the dropcap with this hack
   // create new span element to store the dropcap and append to the beginning of xbbcode
 
   var newSpan = document.createElement('SPAN')
-  var firstLetter = document.createTextNode(xbbcode.childNodes[0].textContent.substr(0,1))
+  var firstLetter = document.createTextNode(xbbcode.childNodes[0].textContent.substr(0,1).toUpperCase())
   newSpan.appendChild(firstLetter)
   newSpan.id = 'drop-cap'
   xbbcode.insertBefore(newSpan, xbbcode.childNodes[0])
