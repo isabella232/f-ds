a58770606a4c5ca229ea6507963f8f8693d9ad50
waazup2 <brandonlchai@gmail.com>
2015-08-09 17:01:41 -0700

    drop cap works when stories begin with images or videos

--- pub/js/dropcap.js
+++ pub/js/dropcap.js
@@ -8,15 +8,30 @@ if ( xbbcode.innerHTML[0] != "<" && xbbcode.innerHTML[0] != "[" ){
   document.getElementsByClassName('definition')[0].childNodes[0].id = 'drop-cap'
 } else if (xbbcode.innerHTML[0] === '<') { // otherwise, implement the dropcap with this hack
   // create new span element to store the dropcap and append to the beginning of xbbcode
+  function isAlpha(xStr){
+    var regEx = /^[a-zA-Z0-9\-]+$/;
+    return xStr.match(regEx);
+  }
+
+  var firstLetter = ""
+  var firstTextBlock
+
+  for (var i = 0; i < xbbcode.childNodes.length; i++) {
+    if ( isAlpha(xbbcode.childNodes[i].textContent.substr(0,1)) ) {
+      var firstTextBlock = xbbcode.childNodes[i]
+      firstLetter = xbbcode.childNodes[i].textContent.substr(0,1).toUpperCase()
+      break
+    }
+  }
 
   var newSpan = document.createElement('SPAN')
-  var firstLetter = document.createTextNode(xbbcode.childNodes[0].textContent.substr(0,1).toUpperCase())
-  newSpan.appendChild(firstLetter)
+  var firstLetterNode= document.createTextNode(firstLetter)
+  newSpan.appendChild(firstLetterNode)
   newSpan.id = 'drop-cap'
-  xbbcode.insertBefore(newSpan, xbbcode.childNodes[0])
+  xbbcode.insertBefore(newSpan, firstTextBlock)
   newSpan.style.textTransform = 'uppercase'
   // delete the leading character in the narrative used as the dropcap
-  xbbcode.childNodes[1].textContent = xbbcode.childNodes[1].textContent.substr(1)
+  firstTextBlock.textContent = firstTextBlock.textContent.substr(1)
 }
 
 var dropcap = document.querySelector("#drop-cap");
