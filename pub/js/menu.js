68cc462ade24d12dc85770d02cb7ba8143cfc299
waazup2 <brandonlchai@gmail.com>
2015-08-25 03:20:17 -0700

    redesigned welcome box on homepage

--- pub/js/menu.js
+++ pub/js/menu.js
@@ -1,11 +1,28 @@
-var menuBtn = document.getElementsByClassName('ion-drag')[0]
-var menu    = document.getElementById('menu')
+var menuBtn        = document.getElementsByClassName('ion-person')[0]
+var desktopMenu    = document.getElementsByClassName('profile-menu-desktop')[0]
+var mobileMenu     = document.getElementsByClassName('profile-menu-mobile')[0]
+// handle menu open
 menuBtn.onclick = function() {
-  if (!menu.className) {
-    menu.style.display = "block"
-    menu.className = "open"
+  if (desktopMenu.className === "profile-menu-desktop") {
+    desktopMenu.style.display = "block"
+    desktopMenu.className = "profile-menu-desktop open"
+    mobileMenu.style.display = "block"
+    mobileMenu.className = "profile-menu-mobile open"
   } else {
-    menu.style.display = "none"
-    menu.className = ""
+    desktopMenu.style.display = "none"
+    desktopMenu.className = "profile-menu-desktop"
+    mobileMenu.style.display = "none"
+    mobileMenu.className = "profile-menu-mobile"
+  }
+}
+// handle menu close
+document.onclick = function(e) {
+  if (desktopMenu.className === "profile-menu-desktop open") {
+    if (e.target.tagName != "LI" && e.target.className != "ion-person") {
+      desktopMenu.style.display = "none"
+      desktopMenu.className = "profile-menu-desktop"
+      mobileMenu.style.display = "none"
+      mobileMenu.className = "profile-menu-mobile"
+    }
   }
 }
