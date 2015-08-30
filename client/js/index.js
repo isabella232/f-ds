9be6ba701a2516d799dbb804d4cb84c982dfde81
Christopher Banh <chrisngobanh@gmail.com>
2015-08-30 15:29:58 -0700

    Removed Hyphenator
    In pull request #75, Brandon said that Hyphenator is no
    longer needed.

--- client/js/index.js
+++ client/js/index.js
@@ -5,7 +5,6 @@ var $ = window.$ = window.jQuery = require('jquery')
 
 require('../lib/d3.v3.min')
 require('../lib/dropcap.min')
-require('../lib/hyphenator.min')
 require('../lib/bbcode/minified/jquery.sceditor.bbcode.min')
 
 require('./bbcode-drop')
