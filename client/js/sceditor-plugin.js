2434be0f975a9af7126249ac12678d138de6a457
ammc <alexander.hdwctw@gmail.com>
2016-12-15 19:26:47 -0800

    add color to WYSIWYG

--- client/js/sceditor-plugin.js
+++ client/js/sceditor-plugin.js
@@ -3,6 +3,6 @@ $(function() {
     // with SCEditor
     $("textarea").sceditor({
         plugins: 'bbcode',
-        toolbar:'bold,italic,underline|size|bulletlist,orderedlist|quote,horizontalrule|image,youtube|source',
+        toolbar:'bold,italic,underline|size,color|bulletlist,orderedlist|quote,horizontalrule|image,youtube|source',
     });
 });
